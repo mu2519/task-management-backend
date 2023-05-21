37f26f77c292303dbcf6d41886b819d9c8be9f0f
Masayuki Usui <usui-masayuki394@g.ecc.u-tokyo.ac.jp>
2023-05-21 17:21:33 +0900

    Update for MySQL

--- models.py
+++ models.py
@@ -5,19 +5,19 @@ from .database import Base
 class SessionManagement(Base):
     __tablename__ = "sessions"
     session_id = Column(Uuid, primary_key=True)
-    username = Column(String, nullable=False)
+    username = Column(String(100), nullable=False)
 
 
 class User(Base):
     __tablename__ = "users"
-    username = Column(String, primary_key=True)
-    hashed_password = Column(String, nullable=False)
+    username = Column(String(100), primary_key=True)
+    hashed_password = Column(String(70), nullable=False)
 
 
 class Task(Base):
     __tablename__ = "tasks"
     id = Column(Uuid, primary_key=True)
-    username = Column(String, nullable=False)
-    title = Column(String, nullable=False)
-    description = Column(String, nullable=False)
+    username = Column(String(100), nullable=False)
+    title = Column(String(100), nullable=False)
+    description = Column(String(1000), nullable=False)
     deadline = Column(DateTime, nullable=True)
